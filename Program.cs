fb5c2545e2376eda13ffdeb7db247eda10b45641
IGYNI <svitmira777@gmail.com>
2024-12-05 10:21:46 +0100

    Added Geter of files by path

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+
+
+    static void Main()
+    {
+        var Path = Console.ReadLine();
+
+        List<string> allFiles = GetAllFiles(Path);
+
+        foreach (string file in allFiles)
+        {
+            Console.WriteLine(file);
+        }
+
+
+    }
+
+    static List<string> GetAllFiles(string directory)
+    {
+        List<string> files = new List<string>();
+
+        try
+        {
+            // Получаем все файлы в текущей директории
+            files.AddRange(Directory.GetFiles(directory));
+
+            // Рекурсивно обходим все подкаталоги
+            foreach (string subDirectory in Directory.GetDirectories(directory))
+            {
+                files.AddRange(GetAllFiles(subDirectory));
+            }
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine($"Нет доступа к папке: {directory}. Ошибка: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при работе с папкой: {directory}. Ошибка: {ex.Message}");
+        }
+
+        return files;
+    }
+
+
+}
